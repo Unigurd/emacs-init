3b7126dd732c0f266e19b05689911787cac6c8c0
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-10-03 23:44:03 +0200

    Update gurd-theme. Forgot what the changes were.

--- custom-themes/gurd-theme.el
+++ custom-themes/gurd-theme.el
@@ -5,7 +5,7 @@
  'gurd
  '(fringe ((t (:background "black"))))
  '(mode-line-buffer-id ((t :inherit sml/filename :foreground nil :background nil)))
- '(mode-line-inactive ((t nil)))
+ '(mode-line-inactive ((t :foreground "gray60" :background "#404045" :inverse-video nil)))
  '(mode-line ((t :foreground "gray60" :background "black" :inverse-video nil)))
  '(sml/global ((t :foreground "gray50" :inverse-video nil)))
  '(sml/modes ((t :inherit sml/global :foreground "White")))
@@ -14,7 +14,7 @@
  '(sml/read-only ((t :inherit sml/not-modified :foreground "DeepSkyBlue")))
  '(persp-selected-face ((t :foreground "ForestGreen" :inherit sml/filename)))
  '(helm-candidate-number ((t :foreground nil :background nil :inherit sml/filename)))
- '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "PfEd" :family "Liberation Mono"))))
- '(mode-line-active ((t (:inherit mode-line :background "gray15")))))
+ '(mode-line-active ((t (:inherit mode-line :background "gray15"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "PfEd" :family "Liberation Mono")))))
 
 (provide-theme 'gurd)
