5767fb01e613307c1d956d4b75f5df81028802e0
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-10-04 21:24:00 +0200

    use-package ace-jump-mode

--- init.el
+++ init.el
@@ -495,6 +495,14 @@
 
 (use-package font-lock-studio)
 
+(use-package ace-jump-mode
+    :bind ("C-S-SPC" . ace-jump-char-mode)
+    :config
+    (setf ace-jump-mode-move-keys
+          (list ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9 ?0
+                ?q ?w ?e ?r ?t ?y ?u ?i ?o ?p
+                ?a ?s ?d ?f ?g ?h ?j ?k ?l
+                ?z ?x ?c ?v ?b ?n ?m)))
 
 
 
