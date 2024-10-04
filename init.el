2422ec26b776a763f503fe9839c41d69ed969fbd
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-10-05 00:01:02 +0200

    use-package vertico

--- init.el
+++ init.el
@@ -184,6 +184,15 @@
   :init
   (savehist-mode))
 
+(use-package vertico
+  ;; :custom
+  ;; Suggestions from https://github.com/minad/vertico?tab=readme-ov-file#configuration
+  ;; (vertico-scroll-margin 0) ;; Different scroll margin
+  ;; (vertico-count 20) ;; Show more candidates
+  ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  ;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :init
+  (vertico-mode))
 
 ;;
 ;;: gurd
