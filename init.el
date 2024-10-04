fc67c0b1588a5b63f790b927ea0c35fbfec32c08
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-10-04 23:20:37 +0200

    Remove code snippet for disabling mouse

--- init.el
+++ init.el
@@ -362,23 +362,6 @@
     (lambda () (rainbow-delimiters-mode 1)))
   (my-global-rainbow-delimiters-mode 1))
 
-;; Back when I didn't use the mouse :')
-(define-minor-mode disable-mouse-mode
-  "A minor-mode that disables all mouse keybinds."
-  :global t
-  :lighter " 🐭"
-  :keymap (make-sparse-keymap))
-
-
-(dolist (type '(mouse down-mouse drag-mouse
-                      double-mouse triple-mouse))
-  (dolist (prefix '("" C- M- S- M-S- C-M- C-S- C-M-S-))
-    ;; Yes, I actually HAD to go up to 7 here.
-    (dotimes (n 7)
-      (let ((k (format "%s%s-%s" prefix type n)))
-        (define-key disable-mouse-mode-map
-          (vector (intern k)) #'ignore)))))
-
 ;;Smart mode line
 (use-package smart-mode-line
   :ensure t
